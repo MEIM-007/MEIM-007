220bd2be5259d2e939f9638dfa735c064318cd84
lixuhua <458412625@qq.com>
2022-10-12 12:20:46 +0800

    改变功能

--- book-service/src/main/java/com/test/service/BookService.java
+++
@@ -1,9 +0,0 @@
-package com.test.service;
-
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.test.entity.Book;
-
-public interface BookService extends IService<Book> {
-
-}
--- user-service/src/main/java/com/test/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.test.service;
-
-import com.test.entity.User;
-
-public interface UserService {
-    User getUserById(int uid);
-}
