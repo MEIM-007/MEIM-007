306048eb2a4a14f9026636e8d2a07c12925358bc
lixuhua <458412625@qq.com>
2022-10-12 13:18:13 +0800

    更新设置

---
+++ goodservice/src/test/java/com/example/goodservice/GoodserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goodservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
