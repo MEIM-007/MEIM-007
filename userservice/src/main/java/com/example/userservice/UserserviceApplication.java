53a6f47ceab9588a7e85445b97cae6f742adbfb9
lixuhua <458412625@qq.com>
2022-10-14 00:03:19 +0800

    gateway功能修复

--- userservice/src/main/java/com/example/userservice/UserserviceApplication.java
+++ userservice/src/main/java/com/example/userservice/UserserviceApplication.java
@@ -3,10 +3,14 @@ package com.example.userservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @MapperScan("com.example.userservice.db.dao")
+@EnableDiscoveryClient
+@EnableFeignClients
 public class UserserviceApplication {
 
     public static void main(String[] args) {
